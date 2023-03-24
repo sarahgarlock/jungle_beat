82185e7ff961f6cd315d096f36eb62b486132afc
Sarah Garlock <garlock.sarah10@gmail.com>
2023-03-23 21:47:38 -0600

    Add a test file

---
+++ spec/node_spec.rb
@@ -0,0 +1,20 @@
+require 'rspec'
+require './lib/node'
+require 'pry'
+
+RSpec.describe Node do
+  it 'exists' do  
+    data = "plop"
+    node = Node.new(data)
+
+    expect(node).to be_a(Node)
+  end
+
+  it 'has data' do
+    data = "plop"
+    node = Node.new(data)
+
+    expect(node.data).to eq("plop")
+  end
+end
+
