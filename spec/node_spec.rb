d1e762c40bb3f051747e0fb9870e5c1bb4b6b084
Sarah Garlock <garlock.sarah10@gmail.com>
2023-03-24 15:24:05 -0600

    Finish adding node test

--- spec/node_spec.rb
+++ spec/node_spec.rb
@@ -4,17 +4,19 @@ require 'pry'
 
 RSpec.describe Node do
   it 'exists' do  
-    data = "plop"
+    data = 'plop'
     node = Node.new(data)
 
     expect(node).to be_a(Node)
   end
 
-  it 'has data' do
-    data = "plop"
+  it 'has data' Node do
+    data = 'plop'
     node = Node.new(data)
 
-    expect(node.data).to eq("plop")
+    expect(node.data).to eq('plop')
   end
+
+
 end
 
