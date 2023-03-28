2e8c24a4324d0cad02f486334b4f741e27d9a67d
Sarah Garlock <garlock.sarah10@gmail.com>
2023-03-28 15:46:56 -0600

    Clean up Jungle Beat rb

--- lib/jungle_beat.rb
+++ lib/jungle_beat.rb
@@ -26,7 +26,8 @@ class JungleBeat
     added_sounds = added_sounds(sounds)
     added_sounds.each do |added_sound|
       list.append(sound)
-
+    end
+  end
  
   # same count method from LinkedList class
   def count
