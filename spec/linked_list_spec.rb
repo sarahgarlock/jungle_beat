d23b9ec086557ffc79f86dceb7dd6d431e845164
Sarah Garlock <garlock.sarah10@gmail.com>
2023-03-27 21:56:57 -0600

    update expect for find method

--- spec/linked_list_spec.rb
+++ spec/linked_list_spec.rb
@@ -85,6 +85,7 @@ RSpec.describe LinkedList do
     expect(list.to_string).to eq ('deep woo shi shu blop')
 
     expect(list.find(2, 1)).to eq('shi')
+    expect(list.find(1, 3)).to eq('woo shi shu')
   end
 
 end
